e20de28d363c7868f16e378aeb4805cfebfad5c2
dell <aditya.chauhan_cs18@gla.ac.in>
2019-09-03 22:04:13 +0530

    lab4-solve4

---
+++ src/solve4.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class solve4 {
+    public static void main(String[] args) {
+        double input1=3.14;
+        double input2=5.0;
+
+         double sum=input1+input2;
+        System.out.println("the sum of two variables="+sum);
+    }
+}
